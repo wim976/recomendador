72fbb9aeb3b423d566891ab24ec56aea3a336d64
wim976 <gcevallos8751@utm.edu.ec>
2025-03-22 01:19:00 -0500

    Actualización de la API

--- api.py
+++ api.py
@@ -140,4 +140,5 @@ def grupo6():
 
 # Iniciar el servidor Flask
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Usa el puerto que Render le asigna
+    app.run(debug=True, host='0.0.0.0', port=port)
